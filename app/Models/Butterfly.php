899b1ea3bfd1bd09caa7b55ae1b1082d94ee7618
Larissacalcanhojusti@gmail.com <Larissacalcanhojusti@gmail.com>
2023-03-15 22:37:57 -0300

    oi

---
+++ app/Models/Butterfly.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Gato extends Model
+{
+    use HasFactory;
+
+    // Indica o nome da tabela do banco de dados
+    protected $table = "butterflies";
+
+    // Indica os campos que podem ser preenchidos pelo usuário
+    protected $fillable = [
+        'nome',
+        'especies',
+        'idade',
+        'sexo',
+        'cor',
+        'significado',
+        'foto'
+    ];
+}
